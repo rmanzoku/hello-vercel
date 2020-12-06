28c5a6bd5f2f0381d31a7b4fb39c91dd35475b3f
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-12-06 11:56:43 +0900

    class component

--- components/accountBottun.tsx
+++ components/accountBottun.tsx
@@ -1,16 +1,37 @@
 import Link from 'next/link'
+import React from 'react'
 
-
-export default function AccountBotton() {
-    return (
-        <div>
-            <Link href="/account">
-                {accountStatus()}
-            </Link>
-        </div>
-    )
+interface Props {
+}
+interface State {
+    locked: boolean
+    status: string
 }
 
-function accountStatus() {
-    return "Unlock"
+export default class AccountBotton extends React.Component<Props, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            locked: true,
+            status: "Lock"
+        }
+    }
+
+    private handleClick = (e: React.MouseEvent) => {
+        this.setState({
+            locked: !this.state.locked,
+            status: this.state.locked ? "Unlock" : "Lock"
+        })
+    }
+
+    render = () => {
+        return (
+            <div>
+                <button onClick={this.handleClick}>
+                    {this.state.status}
+                </button>
+            </div>
+        )
+    }
 }
+
