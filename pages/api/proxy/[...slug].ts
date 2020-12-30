201409cd1123993e90efbecd114fd8f2603fb735
Ryo Manzoku <manzoku.ryo@gmail.com>
2020-12-30 15:24:47 +0900

    api

--- pages/api/proxy/[...slug].ts
+++ pages/api/proxy/[...slug].ts
@@ -1,11 +1,22 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import httpProxy from 'http-proxy';
 
-const target = "https://api.exchangeratesapi.io/"
+const target = "http://static.s3.prod.mch.djty.co.s3-ap-northeast-1.amazonaws.com/"
 
 const proxy = httpProxy.createProxyServer({ target, changeOrigin: true });
 
-export default function handler(req: IncomingMessage, res: ServerResponse) {
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
     req.url = req.url!.replace(new RegExp("^/api/proxy"), "")
-    proxy.web(req, res);
+
+    return new Promise((resolve, reject) => {
+        try {
+            proxy.web(req, res, { proxyTimeout: 5000 }, (e) => {
+                console.warn(e.message)
+            })
+            return resolve
+        } catch (e) {
+            console.warn(e.message)
+            return reject
+        }
+    })
 }
