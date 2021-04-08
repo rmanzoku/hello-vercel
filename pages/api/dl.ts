aaa8d3f0b464fd015462ce9fdfbefc9a8de1449c
Ryo Manzoku <manzoku.ryo@gmail.com>
2021-04-08 23:19:53 +0900

    update

---
+++ pages/api/dl.ts
@@ -0,0 +1,17 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { URL } from 'url'
+
+let ret: number[] = [5011, 5012, 5013, 5031, 5032, 5033, 5041, 5042];
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+    const url = new URL(req.url!, `http://${req.headers.host}`)
+    const ids = url.searchParams.getAll("ids")
+    if (ids.length != 0) {
+        ret = []
+        ids.map((id) => { ret.push(Number(id)) })
+    }
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify(ret))
+}
